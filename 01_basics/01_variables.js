90b9f8ab500b3ac41f986d15c10bc16b99620c87
Nilesh  Kumar Singh <singhraja501@gmail.com>
2025-02-15 10:44:32 +0000

    conversion of datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 439435
+let accountEmail= "nilesh@gmail.com"
+var accountPassword ="12343"
+accountCity = "Jaipur"
+
+let accountState;
+// accountId = 2 // not allowed
+accountEmail = "Hdfc@hc.com"
+accountPassword = "1232342"
+accountCity ="Bangaluru"
+console.table([accountEmail, accountId, accountPassword, accountCity, accountState])
+
+
+/* Prefer not to use var
+    because of issue in block scope and functional scope
+ */
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+//alert("Hello") //we are using node.js not browser
+
+console.log(3 
+    +
+     3)// code readability should be high
+console.log("Nilesh")
+
+let name = "Nilesh"
+let age = 23
+let isLoggedIn = false
+
+//number => 2 to powe 53
+// bigint
+// string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => 
+// symbol
+//object
+
+console.log(typeof "Nilesh");
+console.log(typeof undefined);
