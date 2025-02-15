2a43fa3715d92b7918fd67e6644f553b152933fc
Nilesh  Kumar Singh <singhraja501@gmail.com>
2025-02-15 17:09:35 +0000

    lecture 3 and 4

--- 01_basics/03_conversionOpreation.js
+++ 01_basics/03_conversionOpreation.js
@@ -1,32 +1,57 @@
-// let score = "Nilesh"
-// console.log(typeof score);
-// console.log(typeof(score));
+// // let score = "Nilesh"
+// // console.log(typeof score);
+// // console.log(typeof(score));
 
-// let valueInNumber = Number(score);
+// // let valueInNumber = Number(score);
 
-// console.log(typeof valueInNumber);
-// console.log(valueInNumber)
+// // console.log(typeof valueInNumber);
+// // console.log(valueInNumber)
 
-/* "33 => 33" 
-    "33abc => NaN"
-    true => 1; flase => 0
+// /* "33 => 33" 
+//     "33abc => NaN"
+//     true => 1; flase => 0
 
-*/
-// let isLoggedIn = 1
+// */
+// // let isLoggedIn = 1
 
-// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// // let booleanIsLoggedIn = Boolean(isLoggedIn);
 
-// console.log(booleanIsLoggedIn)
+// // console.log(booleanIsLoggedIn)
 
-/* 
-1 => true
-0 => false
-"Nilesh" => true
-*/
+// /* 
+// 1 => true
+// 0 => false
+// "Nilesh" => true
+// */
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
-console.log(someNumber);
+// let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+// console.log(someNumber);
+
+// ************************ OPREATIONS ************************************************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+console.log(2+2);
+console.log(2-2);
+console.log(2**2);
+console.log(2*2);
+console.log(2/2);
+console.log(2%3);
+
+let str1 = "Hello"
+let str2 =" Nilesh"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + 2);
+console.log(1 + 2 + "2");
+
+console.log((3 + 4)*5 %3);
+
+console.log(+true);
+console.log(+"")
---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,19 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log( null > 0); //false
+console.log( null >= 0); // true
+console.log( null == 0); //false
+
+console.log( undefined == 0);
+
+
+// === strict check , it will not only check values but also checks the datatype
+
+console.log("2" === 2); // false
